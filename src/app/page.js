eb9c87a0c41c60937748aee2d00d97da8afb2f55
vsiago <vsiago23@gmail.com>
2024-02-17 12:18:17 -0300

    tudo correto

--- src/app/page.js
+++ src/app/page.js
@@ -105,11 +105,16 @@ export default function Carta() {
 
           {/* RENDERIZA SUBMENUS PARA BUSCAR TODOS OS SERVICOS */}
           {secretariaSelecionada && (
-            <Submenus
-              servicos={servicos}
-              cartaSelecionada={cartaSelecionada}
-              handleCartaClick={handleCartaClick}
-            />
+            <div className='overflow-x-auto'>
+              <ul className='flex gap-2 mt-3 z-[1000] whitespace-nowrap h-16 items-center'>
+                <p className={`p-1 px-3 rounded-full cursor-pointer h-8 ${!cartaSelecionada ? 'bg-slate-600 text-white' : 'bg-slate-300 text-black'}`} onClick={() => handleCartaClick(null)}>Todos de {secretariaSelecionada.nome}</p>
+                {servicos.map((servico, index) => (
+                  <li onClick={() => handleCartaClick(servico)} key={index}>
+                    <p className={`p-1 px-3 rounded-full cursor-pointer h-8 ${cartaSelecionada === servico ? 'bg-slate-600 text-white' : 'bg-slate-300 text-black'}`}>{servico.nome}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
           )}
 
           {/* CARTAS DOS SERVICOS */}
